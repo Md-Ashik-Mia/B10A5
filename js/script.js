16d828f66c560cc6851d73308d3c56bd2bc66f30
ashik-mia <ashikdeone908@gmail.com>
2025-02-19 21:07:13 +0600

    addion amount sucessfull

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ donationButton.addEventListener("click",()=>{
         console.log(donationButton)
         handleNavigationButtonById('donationButton')
 })
+
+buttons = document.getElementsByClassName("donateNow")
+for (button of buttons){
+        button.addEventListener("click",(e)=>{
+            e.preventDefault();
+            var inputValue= parseInt(e.target.parentNode.firstElementChild.value)
+            var currentButton=e.target.parentNode.parentNode.firstElementChild.firstElementChild.children[1]
+           var cardValue= parseInt(currentButton.innerText)
+           currentButton.innerText=cardValue+inputValue;
+        
+        })
+    }
+// var buttonCard1 = document.getElementById("buttonCard1")
+// buttonCard1.addEventListener("click",(e)=>{
+//       my_modal_4.showModal()
+//         e.preventDefault();
+
+// })
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,21 @@
 function handleNavigationButtonById(id){
     document.getElementById("donationButton").classList.remove("bg-[#B4F461]")
     document.getElementById("historyButton").classList.remove("bg-[#B4F461]")
-    console.log(document.getElementById(id))
+    document.getElementById("donationButton").classList.add("bg-transparent")
+    document.getElementById("historyButton").classList.add("bg-transparent")
+    document.getElementById(id).classList.remove("bg-transparent");
     document.getElementById(id).classList.add("bg-[#B4F461]");
+    showSection(id)
+}
+
+function showSection(id){
+    if(id=='donationButton'){
+        document.getElementById("historySection").classList.add("hidden")
+        document.getElementById("cards-container").classList.remove("hidden")
+    }
+    else{
+        document.getElementById("cards-container").classList.add("hidden")
+        document.getElementById("historySection").classList.remove("hidden")
+    }
+
 }
